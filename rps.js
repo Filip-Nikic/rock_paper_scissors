e2abcb8b10cc3e9e4733b48ad0726643328d5922
Filip-Nikic <filip.nikic@yahoo.com>
2023-08-29 20:31:51 +0200

    Added different version to the branch

--- rps.js
+++ rps.js
@@ -1,53 +1,117 @@
+let compChoice = {Value: ""};
+let playerChoice;
+let compChoiceInt = 0;
+let playerChoiceInt = 0;
+const buttons = document.querySelectorAll('.btn');
 
-function getComputerChoice() {
-	let n= Math.floor(Math.random() * 3 + 1);
-	console.log(n);
-	if (n===1){
-		return ("Rock");
-	} else if(n===2) {
-		return ("Paper");
-	} else {
-		return ("Scissors")
-	}
+let playerScore = 0;
+let compScore = 0;
+
+let audio = new Audio('music/mortal.mp3');
+
+const player = document.querySelector("#player-score");
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector("#comp-score");
+computer.textContent = `Computer Score: ${compScore}`;
+
+const output = document.querySelector("#output");
+output.textContent = "May the Best Person Win!"
+
+
+buttons.forEach((button)=>{button.addEventListener('click',()=>{
+
+    playerChoice = button.id;
+    if (playerChoice == "rock"){
+        playerChoiceInt = 0;
+    }
+    else if (playerChoice == "paper"){
+        playerChoiceInt = 1;
+    }
+    else if (playerChoice == "scissors")
+    {
+        playerChoiceInt = 2;
+    }
+    compChoiceInt = computerPlay(compChoice);
+    playGame();
+    })
+
+})
+
+function computerPlay(compChoice){
+    let choiceNum = Math.floor(Math.random() * 3);
+    if (choiceNum == 0){
+        compChoice.Value = "rock";
+    }
+    else if (choiceNum == 1){
+        compChoice.Value = "paper";
+    }
+    else if(choiceNum == 2){
+        compChoice.Value = "scissors";
+    }
+    return choiceNum;
 }
 
-function playRound(playerSelection, computerSelection) {
-	let p = playerSelection.toLowerCase();
-	if (p==="rock") {
-		if (computerSelection==="Rock") {
-			return ("It's a tie! Rock vs Rock");
-		} else if (computerSelection==="Paper") {
-			return ("You Lose! Paper beats Rock");
-		} else if (computerSelection==="Scissors") {
-			return ("You Win! Rock beats Scissors");
-		}
-	} else if (p==="paper") {
-		if (computerSelection==="Rock") {
-			return ("You Win! Paper beats Rock");
-		} else if (computerSelection==="Paper") {
-			return ("It's a tie! Paper vs Paper");
-		} else if (computerSelection==="Scissors") {
-			return ("You Lose! Scissors beat Paper");
-		}
-	} else if (p==="scissors"){
-		if (computerSelection==="Rock") {
-			return ("You Lose! Rock beat Scissors");
-		} else if (computerSelection==="Paper") {
-			return ("You Win! Scissors beats Paper");
-		} else if (computerSelection==="Scissors") {
-			return ("It's a tie! Scissors vs Scissors");
-		}
-	} else {
-		return ("Please enter a valid entry!");
-	}
+function playRound(){
+        let win_array = [[0, 2, 1], 
+                        [1, 0, 2], 
+                        [2, 1, 0]];
+    let result = win_array[playerChoiceInt][compChoiceInt];
+    if (result == 0){
+    output.textContent = `Its a tie! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
+    }
+    else if (result == 1){
+    output.textContent = `You won! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
+    playerScore++;
+
+    }
+    else if (result == 2){
+    output.textContent = `You lost! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
+    compScore++;
+    }
 }
 
-function game(){
-	for (let i=1; i<=5; i++){
-		const playerSelection = prompt("Please enter, Rock, Paper or Scissors!");
-		const computerSelection = getComputerChoice();
-		console.log(playRound(playerSelection, computerSelection));
-	}
+function playGame(){
+        output.textContent = "Choose Rock, Paper, or Scissors";
+        playRound();
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
+        if (playerScore == 5){
+            let song = new Audio('music/victory.mp3');
+			output.textContent = "You Won the Game! Congrats";
+            playerScore = 0;
+            compScore = 0;
+            player.textContent = `Player Score: ${playerScore}`;
+            computer.textContent = `Computer Score: ${compScore}`;
+			song.play();
+        }
+        else if (compScore == 5){
+            let song = new Audio('music/lose.mp3');
+			output.textContent = "You Lost the game:/ Maybe find something else to do?"
+            playerScore = 0;
+            compScore = 0;
+            player.textContent = `Player Score: ${playerScore}`;
+            computer.textContent = `Computer Score: ${compScore}`;
+			song.play();
+        }
 }
 
-game();
+function music_play() {
+  let image = document.getElementById("m_play");
+  audio.play();
+  document.getElementById("play").style.visibility="hidden";
+  document.getElementById("stop").style.visibility="visible";
+  document.getElementById("play").style.display = 'none'
+  document.getElementById("stop").style.display = 'inline-block';
+}
+
+function music_stop() {
+  let image = document.getElementById("m_stop");
+  audio.pause();
+  document.getElementById("stop").style.visibility="hidden";
+  document.getElementById("play").style.visibility="visible";
+  document.getElementById("stop").style.display = 'none'
+  document.getElementById("play").style.display = 'inline-block';
+  
+}
+
